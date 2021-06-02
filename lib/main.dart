4ef31f7c92bc85335747dbe88ed04faf99866278
Jose g Vargas <ruedagato@gmail.com>
2021-06-01 20:15:22 -0500

    set firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_core/firebase_core.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:trips/User/bloc/bloc_user.dart';
 import 'package:trips/platzi_trips.dart';
@@ -11,20 +14,39 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  final Future<FirebaseApp> _initApp = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.light,
     ));
-    return BlocProvider(
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
+    return FutureBuilder(
+      future: _initApp,
+      builder: (context, snapshot) {
+        print('init firebase');
+        if (snapshot.hasError) {
+          print('error init firebase');
+          print(snapshot.error);
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          print('firebase online');
+        }
+
+        return BlocProvider(
+          child: MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: PlatziTrips(),
           ),
-          home: PlatziTrips(),
-        ),
-        bloc: UserBloc());
+          bloc: UserBloc(),
+        );
+      },
+    );
   }
 }
